ed1fdee4c061e4a438c6662f8b5fb1e84de555a4
viancyrn <viancyverhoeven@gmail.com>
2025-03-22 11:32:56 +0100

    Resolve merge conflict in bam2bed.sh

--- bam2bed.sh
+++ bam2bed.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
-input_bam=/scratch/bash_course/lecture_4/assignment/GSM1606197.bam
-output_dir=~/bam2bed
-bed_file="$output_dir/test.bed"
-filtered_bed_file="$output_dir/test_chr1.bed"
-bedtools bamtobed -i "$input_bam" > "$bed_file"
-awk '$1 == "Chr1"' "$bed_file" > "$filtered_bed_file"
-wc -l "$filtered_bed_file"
-echo "Script completed successfully."
-echo "Your name: [Viancy]"
-=======
 
-set -x
-echo "starting BAM to Bed conversion..."
+echo "Starting BAM to BED conversion.."
 
 if [ $# -ne 2 ]; then
-  echo "Usage: $0 <input_bam_file> <output_directory>"
+  echo "Usage: $0 <input_bam_file> <output_directory>
   exit 1
 fi
 
-intput_bam=$1
+input_bam=$1
 output_dir=$2
 
-mkdir -p $output_dir
+mkdir -p "$output_dir"
 
 source $(dirname $(dirname $(which mamba)))/etc/profile.d/conda.sh
 
-conda activate bam2bed || conda create -n bam2bed bedtools -y && conda activate bam2bed
+base_name=$(basename "$input_bam" .bam)
 
-base_name=$(basename $input_bam .bam)
+bed_file="$output_dir/test.bed"
+bedtools bamtobed -i "$input_bam" > "$bed_file"
 
-bed_file="$output_dir/$base_name.bed"
-bedtools bamtobed -i $input_bam > $bed_file
+filtered_bed_file="$output_dir/${base_name}_chr1.bed"
+grep "^Chr1\t" "$bed_file" > "$filtered_bed_file"
 
-filtered_bed_file="$output`-dir/${base_name}_chr1.bed"
-grep -E '^chr1' $bed_file > $filtered_bed_file
-
-wc -l < $filtered_bed_file > "$output_dir/bam2bed_number_of_rows.txt"
+wc -l < "$filtered_bed_file" > "$output_dir/bam2bed_number_of_rows.txt"
 
 echo "Script completed successfully."
 echo "Your name: [Viancy]" 
