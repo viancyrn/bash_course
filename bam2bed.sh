dd110aa777b5fc9add0246c818d0f164b3cbc783
viancyrn <viancyverhoeven@gmail.com>
2025-03-22 13:35:58 +0100

    Add bam2bed.sh script

--- bam2bed.sh
+++ bam2bed.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
-source $(dirname $(dirname $(which mamba)))/etc/profile.d/conda.sh
-
-/mbshome/vverhoeven/miniforge3/condabin/conda create -n bam2bed -y bedtools
-conda_create_return=$?
-echo "conda create return code: $conda_create_return"
-
-/mbshome/vverhoeven/miniforge3/condabin/conda activate bam2bed
-conda_activate_return=$?
-echo "conda activate return code: $conda_create_return"
-
-echo "Conda environment creation and activation attempted"
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <input_bam_file> <output_directory>"
+  exit 1
+fi
 
 input_bam=$1
 output_dir=$2
 
 mkdir -p "$output_dir"
 
+source $(dirname $(dirname $(which mamba)))/etc/profile.d/conda.sh
+
+conda env list | grep -q 'bam2bed' || conda create -n bam2bed bedtools -y
+
+conda activate bam2bed
+
 base_name=$(basename "$input_bam" .bam)
 
-bed_file="$output_dir/test.bed"
+bed_file="$output_dir/$base_name.bed"
+filtered_bed_file="$output_dir/${base_name}_chr1.bed"
+
 bedtools bamtobed -i "$input_bam" > "$bed_file"
 
-filtered_bed_file="$output_dir/${base_name}_chr1.bed"
-grep "^Chr1\t" "$bed_file" > "$filtered_bed_file"
+if [ ! -s "$bed_file" ]; then
+  echo " Original BED file not generated or is empty!"
+  exit 1
+fi
 
-wc -l < "$filtered_bed_file" > "$output_dir/bam2bed_number_of_rows.txt"
+#grep "^Chr1\t" "$bed_file" > "$filtered_bed_file"
+awk '$1 == "Chr1"' "$bed_file" > "$filtered_bed_file"
+
+if [ ! -s "$filtered_bed_file" ]; then
+  echo "Filtered BED file is empty!"
+  exit 1
+fi
+
+line_count=$(wc -l < "$filtered_bed_file")
+printf "%s\n" "$line_count" > "$output_dir/bam2bed_number_of_rows.txt"
+#echo "$line_count" > "$output_dir/bam2bed_number_of_rows.txt"
+
+cat "$output_dir/bam2bed_number_of_rows.txt"
 
 echo "Script completed successfully."
-echo "Your name: [Viancy]" 
-
+echo "Your name: [Viancy]"
